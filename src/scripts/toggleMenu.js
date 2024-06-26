64837bf688b1bd2d90185dcf021bc618cfdb2008
Kamila Dynysiuk <kamila.dynysiuk@gmail.com>
2024-06-26 08:47:58 +0200

    close menu mobile when click on manu link

--- src/scripts/toggleMenu.js
+++ src/scripts/toggleMenu.js
@@ -2,6 +2,7 @@ export function toggleMenu() {
   const toggleButton = document.querySelector('#menu-toggle');
   const navbar = document.querySelector('.navbar');
   const body = document.querySelector('body');
+  const menuLinks = document.querySelectorAll('.menu__item');
 
   function resizedWindow() {
     const bodyWidth = body.getBoundingClientRect().width;
@@ -16,6 +17,14 @@ export function toggleMenu() {
     navbar.classList.toggle('navbar--active');
     toggleButton.classList.toggle('hamburger--active');
     body.classList.toggle('no-scroll');
+  });
+
+  menuLinks.forEach((element) => {
+    element.addEventListener('click', () => {
+      navbar.classList.remove('navbar--active');
+      toggleButton.classList.remove('hamburger--active');
+      body.classList.remove('no-scroll');
+    });
   });
 
   window.addEventListener('resize', resizedWindow);
